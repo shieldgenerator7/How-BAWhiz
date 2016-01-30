8f10c29c5557b14e358a2a9ca03373c93e003025
Wesley Adams <wadams@wescg.net>
2016-01-30 14:23:40 -0500

    Wes' assets with Intro Dialogue scene.

---
+++ Steve/Assets/Wes Stuff/Scripts/dialogueArrow1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class dialogueArrow1 : MonoBehaviour {
+    public void OnClick()
+    {
+
+       // throw new NotImplementedException();
+
+    }
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	
+	}
+}
---
+++ Steve/Assets/Wes Stuff/Scripts/doorAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorAnimation : MonoBehaviour {
+
+    public Animator animator;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("left"))
+        {
+            animator.SetTrigger("doorOpenTrigger");
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            animator.SetTrigger("doorClosedTrigger");
+        }
+
+    }
+}
